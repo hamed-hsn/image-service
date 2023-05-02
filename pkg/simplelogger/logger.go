4742fe5a82b30f8a389ddbfd467fdf24815a67f3
senator <hamedhsn93@gmail.com>
2023-05-02 12:16:27 +0330

    feat pkg/logger

--- pkg/simplelogger/logger.go
+++ pkg/simplelogger/logger.go
@@ -1,1 +1,34 @@
 package simplelogger
+
+import (
+	"go.uber.org/zap"
+)
+
+type Logger struct {
+	zLogger *zap.Logger
+	sugar   *zap.SugaredLogger
+}
+
+func (l Logger) Error(msg string, keyValue ...interface{}) {
+	l.sugar.Errorw(msg, keyValue...)
+}
+
+func (l Logger) Info(msg string, keyValue ...interface{}) {
+
+	l.sugar.Infow(msg, keyValue...)
+}
+
+func (l Logger) Warning(msg string, keyValue ...interface{}) {
+	l.sugar.Warnw(msg, keyValue...)
+}
+
+func (l Logger) Debug(msg string, keyValue ...interface{}) {
+	l.sugar.Debugw(msg, keyValue...)
+}
+
+func New() *Logger {
+	zlogger, _ := zap.NewProduction()
+	sugar := zlogger.Sugar()
+
+	return &Logger{sugar: sugar}
+}
