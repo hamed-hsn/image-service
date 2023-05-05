de34eb3254cf71c21394088c5f0626b1620c4f23
senator <hamedhsn93@gmail.com>
2023-05-05 14:49:50 +0330

    feat delivery/rest implement get and list apis

--- internal/delivery/rest/v1/get.go
+++ internal/delivery/rest/v1/get.go
@@ -1,18 +1,48 @@
 package v1
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"image_service/internal/controller/server"
+	"image_service/internal/dto"
+	"io"
+	"net/http"
+	"os"
 )
 
 func GetInfoApi(ctrl *server.Controller) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return nil
+		var request dto.GetImageRequest
+		request.Url = c.QueryParam("url")
+		request.CommonKey = c.QueryParam("common_key")
+		fmt.Println(request)
+		resp, err := ctrl.GetImageInfo(c.Request().Context(), request)
+		if err != nil {
+			//TODO only for development phase return error message
+			return c.JSON(http.StatusInternalServerError, err)
+		}
+		return c.JSON(http.StatusOK, resp)
 	}
 }
 
 func GetImageApi(ctrl *server.Controller) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return nil
+		var request dto.GetImageRequest
+		request.Url = c.QueryParam("url")
+		request.CommonKey = c.QueryParam("common_key")
+		resp, err := ctrl.GetImageInfo(c.Request().Context(), request)
+		if err != nil {
+			//TODO only for development phase return error message
+			return c.JSON(http.StatusInternalServerError, err)
+		}
+		file, err := os.Open(resp.Info.LocalPath)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, err.Error())
+		}
+		c.Response().Header().Set("extension", resp.Info.Ext)
+		c.Response().Header().Set("mime-type", resp.Info.MimeType)
+		c.Response().Header().Set("Content-Length", fmt.Sprintf("%d", resp.Info.Size))
+		raw, _ := io.ReadAll(file)
+		return c.Blob(http.StatusOK, resp.Info.MimeType, raw)
 	}
 }
--- internal/delivery/rest/v1/list.go
+++ internal/delivery/rest/v1/list.go
@@ -3,10 +3,26 @@ package v1
 import (
 	"github.com/labstack/echo/v4"
 	"image_service/internal/controller/server"
+	"image_service/internal/dto"
+	"net/http"
 )
 
 func ListApi(ctrl *server.Controller) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return nil
+		var request dto.ListImageRequest
+		if err := c.Bind(&request); err != nil {
+			return c.JSON(http.StatusBadRequest, err.Error())
+		}
+		if request.Page == 0 {
+			request.Page = 1
+		}
+		resp, err := ctrl.ListImages(c.Request().Context(), request)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, err.Error())
+		}
+		if len(resp.Data) == 0 {
+			return c.JSON(http.StatusNotFound, resp)
+		}
+		return c.JSON(http.StatusOK, resp)
 	}
 }
