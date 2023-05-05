822bbadafd03bed88b6619dc4fe2a3e0496c6bdf
senator <hamedhsn93@gmail.com>
2023-05-05 09:54:49 +0330

    feat dto/request

---
+++ internal/dto/requests.go
@@ -0,0 +1,22 @@
+package dto
+
+type ListImageRequest struct {
+}
+
+type ListImageResponse struct {
+}
+
+type GetImageRequest struct {
+}
+
+type GetImageInfoResponse struct {
+}
+
+type GetImageRawResponse struct {
+}
+
+type UploadRequest struct {
+}
+
+type UploadResponse struct {
+}
