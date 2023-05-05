583f25e4c733f43bf08fb9bf9718dfb613a4a244
senator <hamedhsn93@gmail.com>
2023-05-05 15:47:26 +0330

    fix delivery/rest upload

--- internal/delivery/rest/v1/upload.go
+++ internal/delivery/rest/v1/upload.go
@@ -3,10 +3,26 @@ package v1
 import (
 	"github.com/labstack/echo/v4"
 	"image_service/internal/controller/server"
+	"image_service/internal/dto"
+	"net/http"
 )
 
 func UploadImageApi(ctrl *server.Controller) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return nil
+		ff, err := c.FormFile("file")
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, err.Error())
+		}
+		file, err := ff.Open()
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, err.Error())
+		}
+		contentType := ff.Header.Get("Content-Type")
+		//return c.JSON(200, contentType)
+		resp, err := ctrl.UploadImage(c.Request().Context(), dto.UploadRequest{File: file, Size: ff.Size, ContentType: contentType})
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, err)
+		}
+		return c.JSON(http.StatusOK, resp)
 	}
 }
